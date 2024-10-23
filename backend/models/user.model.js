3ac84c17b9b2fb67695ea7ff5c809381ba4ecc34
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-10-23 03:05:44 +0300

    Edited user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -9,35 +9,35 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   cart: [
     {
-      meal: { type: Schema.Types.ObjectId, ref: "Meal"},
-      quantity: { type: Number},
+      meal: { type: Schema.Types.ObjectId, ref: "Meal" },
+      quantity: { type: Number },
     },
   ],
-  orders: [{ type: Schema.Types.ObjectId, ref: "Order"}],
+  orders: [{ type: Schema.Types.ObjectId, ref: "Order" }],
 });
 
-module.exports = mongoose.model("User", userSchema);
-
-userSchema.methods.addToCart = function(product) { 
-  const cartProductIndex = this.cart.items.findIndex(cp => {
-      return cp.productId.toString() === product._id.toString();
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
   });
 
   let newQuantity = 1;
   const updatedCartItems = [...this.cart.items];
 
-  if(cartProductIndex >= 0) {
-      // this.cart.items[cartProductIndex].quantity++;
+  if (cartProductIndex >= 0) {
+    // this.cart.items[cartProductIndex].quantity++;
 
-      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
   } else {
-      updatedCartItems.push({productId: product._id, quantity: 1});
-      // this.cart.items.push({productId: product._id, quantity: 1});
+    updatedCartItems.push({ productId: product._id, quantity: 1 });
+    // this.cart.items.push({productId: product._id, quantity: 1});
   }
-  
-  const updatedCart = {items: updatedCartItems};
+
+  const updatedCart = { items: updatedCartItems };
   this.cart = updatedCart;
 
   return this.save();
-}
+};
+
+module.exports = mongoose.model("User", userSchema);
