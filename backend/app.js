eae6eb541a159ae0fc6f93e5cb9984c87b93a233
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-10-23 03:11:12 +0300

    Edited app.js

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const dotenv = require("dotenv");
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
-const userRoute = require('./routes/user.routes');
+const userRoute = require("./routes/user.routes");
 
 dotenv.config({ path: "./config.env" });
 
@@ -20,22 +18,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 app.use(userRoute);
-
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "It works!",
-  });
-});
+app.use(adminRoutes);
+app.use(restaurantRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
@@ -43,7 +37,6 @@ app.use((error, req, res, next) => {
   const data = error.data;
   res.status(status).json({ message, data });
 });
-
 
 mongoose
   .connect(DB_URI)
