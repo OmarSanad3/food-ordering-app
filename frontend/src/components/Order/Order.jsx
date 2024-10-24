69816b5e4bbec1b966ac3b71659d41bd87cc3d49
Zayd Elnaghy <zaydelnaghy@gmail.com>
2024-10-24 14:07:05 +0300

    fix bug img

--- frontend/src/components/Order/Order.jsx
+++ frontend/src/components/Order/Order.jsx
@@ -1,28 +1,30 @@
 import { useContext, useEffect } from "react";
 
-
 import img from "../../assets/dish1.webp";
 import { cartContext } from "../../context/AddToCartContext";
 import toast from "react-hot-toast";
 
-export default function Order({ mealId,title, image, description, price}) {
-  const { addToCart ,decreamentFromCart, deleteCart} = useContext(cartContext);
+export default function Order({ mealId, title, image, description, price }) {
+  const { addToCart, decreamentFromCart, deleteCart } = useContext(cartContext);
 
   function addProduct() {
     addToCart(mealId);
     console.log("Product added to cart:", mealId);
-    
   }
   function deleteProduct() {
     decreamentFromCart(mealId);
     console.log("Product deleted from cart:", mealId);
   }
 
-
   return (
     <>
       <div className="d-flex pt-2 pb-2 gap-3">
-        <img src={img} alt="" style={{ height: "40px" }} className="me-2" />
+        <img
+          src={image === "https://example.com/topdish.png" ? img : image}
+          alt=""
+          style={{ height: "40px" }}
+          className="me-2"
+        />
         <div className="item-name" style={{ width: "100%" }}>
           <h4 className="fs-6">{title}</h4>
           <p className={`text-black-50`}>{description}</p>
@@ -30,13 +32,9 @@ export default function Order({ mealId,title, image, description, price}) {
         <div className="price d-flex justify-content-end">
           <div style={{ maxWidth: "100%" }}>EGP {price}</div>
           <i
-            className="bi bi-plus-circle-fill text-warning"
+            className="bi bi-plus-circle-fill text-warning cursor-pointer"
             onClick={() => addProduct()}
           ></i>
-          
-
-          
-          
         </div>
       </div>
       <hr className="m-0" />
