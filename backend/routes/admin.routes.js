709424032871dcf8e14bdfff7df6233881cbf602
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-10-24 16:27:33 +0300

    added edit meal logic and delete meal logic

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -6,14 +6,16 @@ const adminController = require("../controllers/admin.controller");
 
 router.post("/add-restaurant", adminController.addRestaurant);
 
-router.post("/update-tags/:restaurantId", adminController.updateTags);
-
-router.post("/add-meal/:restaurantId", adminController.addMeal);
-
-// router.post("/edit-meal/:mealId", adminController.editMeal);
-
 router.post("/edit-restaurant/:restaurantId", adminController.editRestaurant);
 
-// router.delete("/delete-meal/:mealId", adminController.deleteMeal);
+// router.delete("/delete-restaurant/:restaurantId", adminController.deleteRestaurant);
+
+// ================================================
+
+router.post("/add-meal/", adminController.addMeal);
+
+router.post("/edit-meal/:mealId", adminController.editMeal);
+
+router.delete("/delete-meal/:mealId", adminController.deleteMeal);
 
 module.exports = router;
